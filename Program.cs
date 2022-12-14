c13adedd965c951eb08dc674c80dd6d2a08881e3
BjornBEs <bjorn@r66.dk>
2022-12-14 11:33:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GameEngineV
+{
+    class Program
+    {
+        static void Main()
+        {
+            Program program = new();
+            program.Start();
+        }
+        public void Start()
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
