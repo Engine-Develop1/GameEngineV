b0709cb0273478dca58f3dbcec2ac3d4f3555434
BjornBEs <bjorn@r66.dk>
2022-12-14 13:39:20 +0100

    start

---
+++ npcs/dialog/NPCDialog.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngineV.npcs.dialog
+{
+    public class NPCDialog : npcs
+    {
+        public void NpcWL(NPCNames npcNames ,string mess)
+        {
+            Console.WriteLine(ConvToString(npcNames) + mess);
+        }
+        public static bool PlayerInputOptions(string user, string input)
+        {
+            if (user == input)
+                return true;
+            else
+                return false;
+        }
+        public static bool PlayerInputOptions(string user, string[] input)
+        {
+            bool re = true;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (user == input[i])
+                    re = true;
+                else
+                    re = false;
+            }
+            return re;
+        }
+        public static bool PlayerInputOptionsWin(string inputFelt, string input)
+        {
+            Console.Write(inputFelt);
+            if (Console.ReadLine() == input)
+                return true;
+            else
+                return false;
+        }
+        public static bool PlayerInputOptionsWin(string inputFelt, string[] input)
+        {
+            Console.Write(inputFelt);
+            string User = Console.ReadLine();
+            bool re = true;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (User == input[i])
+                    re = true;
+                else
+                    re = false;
+            }
+            return re;
+        }
+        public static string GetRandomLine(string[] lines, int min, int max)
+        {
+            Random random = new();
+            return lines[random.Next(min, max)];
+        }
+    }
+}
---
+++ npcs/npcs.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameEngineV.npcs
+{
+    public enum NPCNames
+    {
+        Player,
+        Elf,
+        Bolvar,
+        TheShopKeeperInStrombard,
+        TheShopKeeperInFirestromb,
+        TheWild
+    }
+    public class npcs
+    {
+        public string ConvToString(NPCNames npc)
+        {
+            switch (npc)
+            {
+                case NPCNames.Player:
+                    return "You:";
+                case NPCNames.Elf:
+                    return "Elfi:";
+                case NPCNames.Bolvar:
+                    return "Bolvar:";
+                case NPCNames.TheShopKeeperInStrombard:
+                    return "The Shop Keeper:";
+                case NPCNames.TheShopKeeperInFirestromb:
+                    return "The Shop Keeper:";
+                case NPCNames.TheWild:
+                    return "The wild:";
+                default:
+                    break;
+            }
+            return "";
+        }
+    }
+}
